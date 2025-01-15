cf747b865dced18131ffa6c0171cec005f0c5e24
Journalwere <142886974+Journalwere@users.noreply.github.com>
2025-01-14 19:48:04 -0800

    Initial Commit

---
+++ eegraw/eegrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  eegrawApp.swift
+//  eegraw
+//
+//  Created by Elijah R on 2025/1/14.
+//
+
+import SwiftUI
+
+@main
+struct eegrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ eegrawTests/eegrawTests.swift
@@ -0,0 +1,17 @@
+//
+//  eegrawTests.swift
+//  eegrawTests
+//
+//  Created by Elijah R on 2025/1/14.
+//
+
+import Testing
+@testable import eegraw
+
+struct eegrawTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
